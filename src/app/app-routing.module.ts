5acc69b62de9456c1a17241ded78bc1c6d4e8e7c
harik31197 <52651351+harik31197@users.noreply.github.com>
2019-11-12 09:53:08 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
+import { NotFoundComponent } from './error-pages/not-found/not-found.component';
+
 
 
 const routes: Routes = [
   {path: '', redirectTo: '/login', pathMatch: 'full'},
- { path:'login',component: LoginComponent }
+ { path:'login',component: LoginComponent },
+ { path: '404', component : NotFoundComponent},
+ { path: '**', redirectTo: '/404', pathMatch: 'full'}
 ];
 
 @NgModule({
