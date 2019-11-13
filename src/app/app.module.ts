321c6ee2568d51f1718aa5373edb5993db4a733b
harik31197 <52651351+harik31197@users.noreply.github.com>
2019-11-13 16:43:23 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './error-pages/not-found/not-found.component';
+import { HttpClientModule,HttpClient} from '@angular/common/http';
+import { MaterialModule } from './material.module';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    NotFoundComponent
+    AppComponent,    
+    NotFoundComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
+   BrowserModule,
+   AppRoutingModule,
+   FormsModule,
+   HttpClientModule,
+   MaterialModule,
+   BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
