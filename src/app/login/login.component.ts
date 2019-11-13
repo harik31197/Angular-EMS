e48a6a36fb4ca7fc5a1ca06bd77b1e6b9170eb70
harik31197 <52651351+harik31197@users.noreply.github.com>
2019-11-13 16:43:42 +0530

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from '../login.service';
+import {UserLogin} from '../data/login';
+import { NgForm, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router,private loginservice:LoginService) { }
+employeeLogin:UserLogin = {
 
+    username: '',
+    password: '',
+    granttype:'password'
+};
+
+errorStatus = false;
+errorMessage = '';
   ngOnInit() {
+  }
+  onSubmit(loginpage :NgForm){
+    console.log('login click works');
+    if(loginpage.valid)
+    {
+      console.log("form valid");
+      this.loginservice.inLoginPage(this.employeeLogin);
+    }
+    else
+    {
+      this.errorStatus = true;
+      this.errorMessage = 'Error Encountered';
+    }
   }
 
 }
