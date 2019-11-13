8e539f4591a2695b1783c5efd38dc4685795c2d5
harik31197 <52651351+harik31197@users.noreply.github.com>
2019-11-13 16:43:27 +0530

    Update login.service.ts

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { UserLogin } from './data/login';
+import { FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,8 @@ export class LoginService {
 
   public inLoginPage(login:UserLogin)
   {
-    const body = `username=${login.username}&password=${login.password}&grant_type=${login.granttype}`;
+    console.log('in InLogin()');
+    const body = `username=${login.username} &password=${login.password} &grant_type=${login.granttype}`;
     return this.http.post('https://localhost:44374/token',body);
   }
   
