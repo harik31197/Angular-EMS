a4b5bf73add2167f8d3903c55603b40909ec3bd5
harik31197 <52651351+harik31197@users.noreply.github.com>
2019-11-14 19:33:39 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import { NotFoundComponent } from './error-pages/not-found/not-found.component';
 import { BackgroundComponent } from './background/background.component';
+import { LeaveComponent } from './leave/leave.component';
 
 
 
@@ -11,6 +12,7 @@ const routes: Routes = [
  { path:'login',component: LoginComponent },
  { path:'bg',component: BackgroundComponent }, 
  { path: '404', component : NotFoundComponent},
+ { path: 'leave', component : LeaveComponent},
  { path: '**', redirectTo: '/404', pathMatch: 'full'}
 ];
 
