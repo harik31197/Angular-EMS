434ed4be1ebd513392b6fa92e1acd7b468f69bf5
harik31197 <52651351+harik31197@users.noreply.github.com>
2019-12-02 13:53:10 +0530

    Update navbar.component.ts

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatMenuTrigger } from '@angular/material/menu';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
+
+
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+ isAdmin = localStorage.getItem('empid');
+  
+  @ViewChild(MatMenuTrigger, {static: true}) trigger: MatMenuTrigger;
+
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+  onLogout()
+  {
+    localStorage.clear();
+    this.router.navigate(['/login']);
   }
 
 }
