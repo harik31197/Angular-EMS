e1fc1bd71ceff3e259e2a2da7f980ca67b603471
harik31197 <52651351+harik31197@users.noreply.github.com>
2019-12-02 13:52:24 +0530

    Create leave.ts

---
+++ src/app/data/leave.ts
@@ -0,0 +1,13 @@
+export class Leave
+{
+    from_date:Date;
+    to_date:Date;
+    from_session:number;
+    to_session:number;
+    leave_description:string;     
+    leave_type_type_id:number;
+    apply_to:string; 
+    leave_type:string;
+    
+
+}
