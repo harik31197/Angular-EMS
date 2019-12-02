81d293ee3e4956f89fe1b5de1086214afa41124f
harik31197 <52651351+harik31197@users.noreply.github.com>
2019-12-02 13:52:18 +0530

    Create contacts.ts

---
+++ src/app/data/contacts.ts
@@ -0,0 +1,14 @@
+export class EmpCont
+{
+    id:number;
+    Name:string;
+    email:string;
+    birthday:Date;
+    bloodgroup:string;
+    doj:Date;
+    department: string;
+    designation:string;
+    username : string;
+    age:number;    
+
+}
