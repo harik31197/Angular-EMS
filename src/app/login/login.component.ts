631e38fabdc85c95a3bba5d14f1ba50f48343a83
harik31197 <52651351+harik31197@users.noreply.github.com>
2019-12-02 13:52:59 +0530

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginService } from '../login.service';
 import {UserLogin} from '../data/login';
-import { NgForm, FormGroup, FormControl } from '@angular/forms';
-
+import { NgForm, FormGroup, FormControl,FormBuilder, Validators} from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -11,30 +10,92 @@ import { NgForm, FormGroup, FormControl } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router,private loginservice:LoginService) { }
-employeeLogin:UserLogin = {
-
-    username: '',
-    password: '',
-    granttype:'password'
-};
-
+empLogin: UserLogin =
+{
+  username:'',
+  password:'',  
+  granttype:'password',
+  
+}
+empID = null;
 errorStatus = false;
 errorMessage = '';
+
+  constructor(private router: Router,private loginservice:LoginService) { }
   ngOnInit() {
+    
   }
-  onSubmit(loginpage :NgForm){
+
+
+  onHttpError(error: any) {
+    console.log('error:', error);
+    this.errorStatus = true;
+    this.errorMessage = error.error.error;
+    console.log(this.errorMessage);
+  }
+
+ /* onSubmit(form:NgForm)
+  {
+    this.authservice.login(this.empLogin).subscribe(
+      data=>{
+        this.tokenparams = data;
+        this.authservice.AccessToken = this.tokenparams.access_token;
+        this.router.navigate(['/bg']);
+      }
+
+    )
+  }*/
+
+  onSubmit(form:NgForm)
+  {
+    console.log(this.empLogin);    
+      this.loginservice.inLoginPage(this.empLogin).subscribe(
+      result=>{
+      console.log(result);
+      localStorage.setItem('token',result.access_token);
+      
+      this.router.navigate(['/dashboard']);
+    },
+      error=>this.onHttpError(error)
+    );
+
+    this.loginservice.getEmpID(this.empLogin.username).subscribe(
+      result=>{
+        console.log(result);
+        localStorage.setItem('empid',result);
+        
+      }
+    );
+   
+ 
+  }
+  redirect()
+  {
+    this.router.navigate(['/forgotpassword']);
+  }
+    /*this.getLoginValues();
     console.log('login click works');
-    if(loginpage.valid)
+
+    if(this.loginPage.valid)
     {
-      console.log("form valid");
-      this.loginservice.inLoginPage(this.employeeLogin);
+      console.log("Valid!!!");
+      this.loginservice.inLoginPage(this.employeeLogin).subscribe (
+      result=>{
+      localStorage.setItem('token',result);
+      this.router.navigate(['/addemployee']);
+    },
+    error=>this.onHttpError(error))
+     
     }
     else
     {
-      this.errorStatus = true;
-      this.errorMessage = 'Error Encountered';
-    }
-  }
+      this.errorStatus=true;
+      this.errorMessage="Errors Encountered";
+    }*/
+  }  
 
-}
+
+    
+  
+
+
