bbba17ae62357259fd1a7cb8ba5ca084d84019d9
harik31197 <52651351+harik31197@users.noreply.github.com>
2019-12-02 13:52:20 +0530

    Create employee.ts

---
+++ src/app/data/employee.ts
@@ -0,0 +1,27 @@
+export class Employee
+{
+
+
+    emp_id:number;
+    first_name:string;
+    last_name:string;
+    emp_email:string;
+    emp_bloodgroup:string;
+    emp_joiningdate:Date;
+    dob: Date;
+    emp_dept:string;
+    emp_desig:string;
+    username :string;
+    emp_age:number;
+    /*id:number;
+    Name:string;
+    email:string;
+    birthday:Date;
+    bloodgroup:string;
+    doj:Date;
+    department: string;
+    designation:string;
+    username : string;
+    age:number;*/
+    
+}
