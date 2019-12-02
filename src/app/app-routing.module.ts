077adbaae59516866e4fd5e7675273fe58b51eb6
harik31197 <52651351+harik31197@users.noreply.github.com>
2019-12-02 13:51:49 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,11 @@ import {LoginComponent} from './login/login.component';
 import { NotFoundComponent } from './error-pages/not-found/not-found.component';
 import { BackgroundComponent } from './background/background.component';
 import { LeaveComponent } from './leave/leave.component';
+import { AddemployeeComponent } from './addemployee/addemployee.component';
+import { AttendanceComponent } from './attendance/attendance.component';
+import { VerifyemployeeComponent } from './verifyemployee/verifyemployee.component';
+import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 
@@ -13,6 +18,11 @@ const routes: Routes = [
  { path:'bg',component: BackgroundComponent }, 
  { path: '404', component : NotFoundComponent},
  { path: 'leave', component : LeaveComponent},
+ { path: 'addemployee', component : AddemployeeComponent},
+ { path: 'attendance', component : AttendanceComponent},
+ { path: 'VerifyAccount/:id', component : VerifyemployeeComponent},
+ { path: 'forgotpassword', component : ForgotpasswordComponent},
+ { path: 'dashboard',component : DashboardComponent},
  { path: '**', redirectTo: '/404', pathMatch: 'full'}
 ];
 
