88a55f0d39da481c02ce741315a059de147cb8ee
Harikrishnan <52651351+harik31197@users.noreply.github.com>
2020-03-30 18:09:56 +0530

    Update forgotpassword.component.ts

--- src/app/forgotpassword/forgotpassword.component.ts
+++ src/app/forgotpassword/forgotpassword.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ForgotPassword} from '../data/forgotpassword'
 import { NgForm } from '@angular/forms';
 import { LoginService } from '../login.service';
+import { MatSnackBar, MatSnackBarConfig, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material';
 
 @Component({
   selector: 'app-forgotpassword',
@@ -10,21 +11,46 @@ import { LoginService } from '../login.service';
 })
 export class ForgotpasswordComponent implements OnInit {
 
-  constructor(private loginservice:LoginService) { }
+  smessage: string = 'Reset Link Sent';
+  actionButtonLabel: string = 'Close';
+  action: boolean = true;
+  setAutoHide: boolean = true;
+  autoHide: number = 2000;
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom'
+
+  constructor(private loginservice:LoginService,public snackBar: MatSnackBar) { }
 
   empdata: ForgotPassword = 
   {
     email:'',
-    empid:null
+    emp_id:null
   };
+  errorStatus=false;
+  errorStatusMessage="";
+
 
   ngOnInit() {
   }
 onSubmit(form:NgForm)
 {
-  this.loginservice.forgotPassword(this.empdata,this.empdata.empid).subscribe(
-    result=>console.log('Email Sent'),
-    error=>console.log('error')
+  console.log(this.empdata);  
+  this.loginservice.forgotPassword(this.empdata).subscribe(
+    result=>console.log('Email Sent',result),
+    error=>this.onHttpError(error)
   );
+  let config = new MatSnackBarConfig();
+    config.verticalPosition = this.verticalPosition;
+    config.horizontalPosition = this.horizontalPosition;
+    config.duration = this.setAutoHide ? this.autoHide : 0;
+    this.snackBar.open(this.smessage, this.action ? this.actionButtonLabel : undefined, config);
+    form.resetForm();
 }
+onHttpError(errorResponse:any)
+  {
+  console.log('error: ',errorResponse);
+  this.errorStatus=true;
+  this.errorStatusMessage=errorResponse.error.error;  
+  }
 }
+
