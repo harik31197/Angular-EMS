2f1d8f3524d5a3f844aa94c22df3e47e381fc422
Harikrishnan <52651351+harik31197@users.noreply.github.com>
2020-03-30 18:10:14 +0530

    Update login.service.ts

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -9,6 +9,7 @@ import { Employee } from './data/employee';
 import { Leave } from './data/leave';
 import { VerifyEmployee } from './data/verifyemployee';
 import { ForgotPassword } from './data/forgotpassword';
+import { Reset } from './data/reset';
 
 @Injectable({
   providedIn: 'root'
@@ -42,20 +43,33 @@ export class LoginService {
   }
 
  postLeaveForm(applyLeave:Leave):Observable<any>
- {
-   this.id=localStorage.getItem('empid');
-   this.empid=parseInt(this.id);
-   const body = `from_date=${applyLeave.from_date}&to_date=${applyLeave.to_date}&from_session=${applyLeave.from_session}&to_session=${applyLeave.to_session}&leave_description=${applyLeave.leave_description}&leave_typt_type_id=${applyLeave.leave_type_type_id}&apply_to=${applyLeave.apply_to}`;
-   return this.http.post('https://localhost:44374/api/leave/'+this.empid,body);
+ { 
+   //const body = `from_date=${applyLeave.from_date}&to_date=${applyLeave.to_date}&from_session=${applyLeave.from_session}&to_session=${applyLeave.to_session}&leave_description=${applyLeave.leave_description}&leave_typt_type_id=${applyLeave.leave_type_type_id}&apply_to=${applyLeave.apply_to}`;
+   return this.http.post('https://localhost:44374/api/leave/'+applyLeave.Employee_emp_id,applyLeave);
  }
 
- verifyAccount(empInstance:VerifyEmployee,id:number)
+ verifyAccount(empInstance:VerifyEmployee,id:number):Observable<any>
  {
    return this.http.post('https://localhost:44374/api/signup/addemployee/VerifyAccount/'+id,empInstance);
  }
- forgotPassword(empdata:ForgotPassword,id:number)
+ forgotPassword(empdata:ForgotPassword):Observable<any>
  {
-  return this.http.post('https://localhost:44374/api/forgotpassword/reset/'+id,empdata);
+  return this.http.post('https://localhost:44374/api/forgotpassword',empdata);
+ }
+ resetthePassword(resetPassword:Reset,id:number):Observable<any>
+ {
+   return this.http.post('https://localhost:44374/api/forgotpassword/reset/'+id,resetPassword);
+ }
+ returnBalanceLeaves(apply:Leave,id:number):Observable<any>
+ {
+   console.log("In Balance");   
+   console.log(apply.leave_type_type_id);
+   console.log(id);
+   return this.http.get('https://localhost:44374/api/leave/'+id+'/'+apply.leave_type_type_id);
+ }
+ returnName(id:number):Observable<any>
+ {
+   return this.http.get('https://localhost:44374/api/getname/'+id);
  }
   
 }
